3a152d7d91574e1bbec4c45d3d9178504a2a6059
flashrifle <flashrifle@gmail.com>
2023-11-22 17:03:10 +0900

    study: 배열 기본, 배열 예제1

--- src/_03_array/Ex_1.java
+++ src/_03_array/Ex_1.java
@@ -6,7 +6,19 @@ public class Ex_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("5개의 정수를 입력 하세요");
-        int[] nums;
+        int[] nums = new int[5];
+        nums[0] = sc.nextInt();
+        nums[1] = sc.nextInt();
+        nums[2] = sc.nextInt();
+        nums[3] = sc.nextInt();
+        nums[4] = sc.nextInt();
 
+        int result = 0;
+
+        for (int sum : nums) {
+            result += sum;
+        }
+        System.out.println("평균 : " + (double) result / 5) ;
+        sc.close();
     }
 }
